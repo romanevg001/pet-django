7743276eceac49f49225394ec3166d852d36e171
Natali Donova <ndonova@yandex.ru>
2026-01-16 14:12:24 +0300

    +

--- petDjango/shopapp/models.py
+++ petDjango/shopapp/models.py
@@ -2,6 +2,10 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Product(models.Model):
+    class Meta:
+        ordering = ['name','price']
+        #db_table = ''
+        #verbose = 'products'
     name = models.CharField(max_length=100)
     description = models.TextField(null=False, blank=True)
     price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
