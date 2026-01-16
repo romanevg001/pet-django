c01d095a2897bed6d86dfb66012c715f0d97d032
Natali Donova <ndonova@yandex.ru>
2026-01-16 18:36:07 +0300

    +

--- petDjango/shopapp/admin.py
+++ petDjango/shopapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import Product
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = "pk","name","description_short","price","discount"
+    list_display_links = "pk","name"
+    ordering = "pk","name"
+    search_fields = "name",'description'
+
+    def description_short(self, obj:Product) -> str:
+        if len(obj.description) > 48:
+            return obj.description[:48] + '...'
+        return obj.description
+
 # Register your models here.
+#admin.site.register(Product, ProductAdmin)
