9c1db3962105253ac4df638865de4164be32534f
Natali Donova <ndonova@yandex.ru>
2026-01-26 15:45:00 +0300

    +

---
+++ petDjango/petDjango/app.py
@@ -0,0 +1,5 @@
+import torch  # импорт функциональности PyTorch
+
+print(torch.__version__)  # выводим версию PyTorch
+
+print(torch.cuda.is_available())  # проверяем доступность дискретной видеокарты
